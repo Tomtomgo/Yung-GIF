46144fe971ec1245877af150f4158356e82a2f04
Tom Aizenberg <tom.aizenberg@gmail.com>
2014-02-17 15:13:56 +0100

    Make Pingu better

--- bg.js
+++ bg.js
@@ -7,28 +7,26 @@ function Pingu(max_size, name){
     /*
         Pingu is a disk-persisted fixed-length fifo buffer. Schnoo schnoo.
     */
-    var name = "YUNG_GIF_"+name;
-
-    // exists list already?
-    this.lst = (localStorage[name] == undefined ? [] : JSON.parse(localStorage[name]));
-    
+    this.space_name = "YUNG_GIF_"+name;
+    this.max_size = max_size;
+    this.lst = (localStorage[this.space_name] == undefined ? [] : JSON.parse(localStorage[this.space_name]));
     this.length = this.lst.length;
 }
 
 // Array(-like) methods
-Pingu.prototype.push = function(e){
-    if(this.lst.length >= max_size){
+Pingu.prototype.unshift = function(e){
+    if(this.lst.length >= this.max_size){
         this.lst.pop();
     }
-    this.lst.push(e);
+    this.lst.unshift(e);
     this.length = this.lst.length;
-    localStorage[name] = JSON.stringify(this.lst);
+    localStorage[this.space_name] = JSON.stringify(this.lst);
 };
 
 Pingu.prototype.pop = function(){
     e = this.lst.pop();
     this.length = this.lst.length;
-    localStorage[name] = JSON.stringify(this.lst);
+    localStorage[this.space_name] = JSON.stringify(this.lst);
     return e;
 };
 
@@ -36,45 +34,44 @@ Pingu.prototype.concat = function(e){
     return this.lst.concat(e.lst);
 };
 
-window.YUNG_GIF = {}
-window.YUNG_GIF.icons = {one: 'crapicon.png', omg: 'omgicon.png', sad: 'sadboy.png'};
-window.YUNG_GIF.seen = new Pingu(100, 'seen')
-window.YUNG_GIF.unseen = new Pingu(25, 'unseen')
-window.YUNG_GIF.running = null
-
-// If icon is clicked, check for new and show.
-chrome.browserAction.onClicked.addListener(function(){window.YUNG_GIF.check_new(true)});
+function Yung_GIF(){
+    this.icons = {one: 'crapicon.png', omg: 'omgicon.png', sad: 'sadboy.png'};
+    this.seen = new Pingu(100, 'seen')
+    this.unseen = new Pingu(25, 'unseen')
+    this.running = null
+}
 
 // bruk() shows unseen gifs.
-window.YUNG_GIF.bruk = function(){
-    while(window.YUNG_GIF.unseen.length > 0){
-        gif_url = window.YUNG_GIF.unseen.pop()
+Yung_GIF.prototype.bruk = function(){
+    while(this.unseen.length > 0){
+        gif_url = this.unseen.pop()
         chrome.tabs.create({url: gif_url});
-        window.YUNG_GIF.seen.push(gif_url);
+        this.seen.unshift(gif_url);
     }
-    chrome.browserAction.setIcon({path: window.YUNG_GIF.icons.one});
+    chrome.browserAction.setIcon({path: this.icons.one});
     chrome.browserAction.setTitle({title: "wait more..."});
     chrome.browserAction.setBadgeText({text: ""});
 }
 
 // Check Reddit for new content and notify if that's the case
-window.YUNG_GIF.check_new = function(do_bruk){
+Yung_GIF.prototype.check_new = function(do_bruk){
+    var me = this;
     $.getJSON('http://api.reddit.com/r/gifs', function(yolo){
 
-        // Iterate in reverse order so the last opened tab is always the hottest
+        // keep the hottest at the beginning so if new GIFs are added, hottest ones stay
         $.each(yolo.data.children.reverse(), function(i,e){
-            if(window.YUNG_GIF.seen.concat(window.YUNG_GIF.unseen).indexOf(e.data.url) == -1) {
-                window.YUNG_GIF.unseen.push(e.data.url)
+            if(me.seen.concat(me.unseen).indexOf(e.data.url) == -1) {
+                me.unseen.unshift(e.data.url)
             }
         });
-                
+        
         // is there something new?
-        if(window.YUNG_GIF.unseen.length > 0){
-            chrome.browserAction.setIcon({path: window.YUNG_GIF.icons.omg});
+        if(me.unseen.length > 0){
+            chrome.browserAction.setIcon({path: me.icons.omg});
             chrome.browserAction.setTitle({title: "NEW GIFS FOR U"});
-            chrome.browserAction.setBadgeText({text: window.YUNG_GIF.seen.length});
+            chrome.browserAction.setBadgeText({text: ""+me.unseen.length});
         }else{
-            chrome.browserAction.setIcon({path: window.YUNG_GIF.icons.one});
+            chrome.browserAction.setIcon({path: me.icons.one});
             chrome.browserAction.setTitle({title: "wait more..."});
             chrome.browserAction.setBadgeText({text: ""});
         }
@@ -82,13 +79,18 @@ window.YUNG_GIF.check_new = function(do_bruk){
         console.log("Checked for new gifs at "+Date())
         
         // Show new GIFs if requested
-        if(do_bruk){window.YUNG_GIF.bruk();}
+        if(do_bruk){me.bruk();}
 
     }).error(function(){
-        chrome.browserAction.setIcon({path: window.YUNG_GIF.icons.sad});
+        chrome.browserAction.setIcon({path: me.icons.sad});
         chrome.browserAction.setTitle({title: "Cannot connect to Reddit."});
     });
 }
+
+window.YUNG_GIF = new Yung_GIF();
+
+// If icon is clicked, check for new and show.
+chrome.browserAction.onClicked.addListener(function(){window.YUNG_GIF.check_new(true)});
 
 // Set alarm to check for new gifs every 5 minutes
 chrome.alarms.create("just_checkin", {periodInMinutes: 5.0});
